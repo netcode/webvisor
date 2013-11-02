971d97388938a41d24fe6ac5d794eaea757dc362
eslam salem <netcodez@gmail.com>
2013-11-02 10:28:06 +0200

    structure

---
+++ js/draw.js
@@ -0,0 +1,9 @@
+var draw = function (){
+	'sdpod':function(){
+		var _this = this;
+		_this.meh
+	},
+	meh:'l'
+}
+
+draw.sdpod()
---
+++ js/sidebar.js
@@ -0,0 +1,11 @@
+/*
+var draw = function (){
+	'sdpod':function(){
+		var _this = this;
+		_this.meh
+	},
+	meh:'l'
+}
+
+draw.sdpod()
+*/
