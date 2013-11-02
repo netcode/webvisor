8f603aaaa2609d3072dcd9e8b140d60312cfbaef
Mohamed Fathi <fat7i.wp@gmail.com>
2013-11-02 15:52:52 +0200

    d

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,7 +3,7 @@ var showSidebar = {
 		
 		var html ='';
 		/*header*/
-		html +='<div class="WebvisorApp">';
+		html +='<div class="WebvisorApp" id="WebvisorApp">';
 		html += '<div class="header">';
 		html += '<div class="title">';
 		html += '<a href="/" class="logo"><img src="https://cdn1.iconfinder.com/data/icons/free-large-boss-icon-set/64/Wizard.png"></a>';
@@ -12,25 +12,13 @@ var showSidebar = {
 		html += '</div>';
 		html += '<br /><br />';
 		html += '<br /><br />';
-		html += '<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Add Comment </button>';
+		html += '<button type="button" id="wa_add_comment" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Add Comment </button>';
 		
 		/*content*/
 		html += '<div class="content">';
 		html += '<div class="posts">';
 		
 		html += this.get_posts();
-		html += this.get_posts();
-		html += this.get_posts();
-		html += this.get_posts();
-		html += this.get_posts();
-		html += this.get_posts();
-		html += this.get_posts();
-		html += this.get_posts();
-		html += this.get_posts();
-		html += this.get_posts();
-		html += this.get_posts();
-		html += this.get_posts();
-		
 		html +='</div>'; /* /:posts*/
 		html +='</div>'; /* /:content*/
 		html +='</div>'; /* /:wa*/
@@ -39,28 +27,34 @@ var showSidebar = {
 		jQuery( document ).ready(function( $ ) {
 			$('body').prepend(html);
 			$('.WebvisorApp .posts').css('height', $(document).height());
+			$('#wa_add_comment').click(function(){
+				login.showToolbar();
+			});
 		});
 	},
 	'get_posts':function() {
 		var html = '';
 		
-		html += '<div id="TODO_userID" class="post post-NaN cold">';
-    	html += '<div class="post-author">';
-      	html += '<a href="https://twitter.com/MohmedFat7i" class="has-tooltip" target="_blank">';
-        html += '<span class="tooltip"><span>Added by <font>@MohmedFat7i</font></span></span>';
-        html += '<img src="https://pbs.twimg.com/profile_images/3393792058/add89b9d945edd13b04f292410cfaf41_bigger.jpeg">';
-        html += '</a>';
-    	html += '</div>';
-	    html += '<div class="post-content">';
-	    html += '<div class="post-info">';
-	    html += '<h3 class="post-heading">';
-	    html += '<span class="post-title" target="_blank">Mohamed Fathi</span>';
-	    html += '</h3>';
-	    html += '<p class="post-text">lorem ipsum sit dolor blah blah, lorem ipsum sit dolor blah blah, lorem ipsum sit dolor blah blah, </p>';
-	    html += '</div>';
-	    html += '</div>';
-	    html += '</div>';
-	    
+		var website = window.btoa(window.location.host)
+		var myDataRef = new Firebase('https://webvisor.firebaseio.com/'+website);
+		
+		myDataRef.on('value', function(snapshot) {
+			html += '<div id="'+snapshot.val().user.id+'" class="post post-NaN cold">';
+	    	html += '<div class="post-author">';
+	      	html += '<a href="https://twitter.com/MohmedFat7i" class="has-tooltip" target="_blank">';
+	        html += '<img src="'+snapshot.val().user.img+'">';
+	        html += '</a>';
+	    	html += '</div>';
+		    html += '<div class="post-content">';
+		    html += '<div class="post-info">';
+		    html += '<h3 class="post-heading">';
+		    html += '<span class="post-title" target="_blank">'+snapshot.val().user.name+'</span>';
+		    html += '</h3>';
+		    html += '</div>';
+		    html += '</div>';
+		    html += '</div>';
+		});
+	
 	    
 	    
 	    return html;
